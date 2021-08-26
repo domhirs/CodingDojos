0da7434cacbc9b06f878a0e5786a2d2aa3fbec68
Dominik Hirsch <dhirsch@econ-tec.de>
2021-08-26 14:04:24 +0200

    added constants for string representations like newline and emptystring

---
+++ src/main/java/de/econ_tec/String/Constants/Constants.java
@@ -0,0 +1,13 @@
+package de.econ_tec.String.Constants;
+
+public class Constants {
+    /**
+     * Contains an empty String.
+     */
+    public final static String STRING_EMPTY = "";
+
+    /**
+     * Contains the newline determined by the operating system.
+     */
+    public final static String NEWLINE = System.lineSeparator();
+}
